457924bbdeffa57a2f0b965c8469fd8e2a8c42dd
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-09-10 07:13:18 -0400

    MVP part 1 binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,6 +9,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,69 +19,98 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+
+        new_node = BSTNode(value)
+
+        if value >= self.value:
+            if self.right == None:
+                self.right = new_node
+                return value
+            else:
+                self.right.insert(value)
+        if value < self.value:
+            if self.left == None:
+                self.left = new_node
+                return value
+            else:
+                self.left.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
+
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+        elif target > self.value and self.right:
+            return self.right.contains(target)
+        elif target < self.value and self.left:
+            return self.left.contains(target)
+        else:
+            return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right == None:
+            return self.value
+        else:
+            return self.right.get_max()
 
     # Call the function `fn` on the value of each node
+
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if self.right:
+            self.right.for_each(fn)
+        if self.left:
+            self.left.for_each(fn)
+    # * Part 2 -----------------------
 
-    # Part 2 -----------------------
+#     # Print all the values in order from low to high
+#     # Hint:  Use a recursive, depth first traversal
+#     def in_order_print(self):
+#         pass
 
-    # Print all the values in order from low to high
-    # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self):
-        pass
+#     # Print the value of every node, starting with the given node,
+#     # in an iterative breadth first traversal
+#     def bft_print(self):
+#         pass
 
-    # Print the value of every node, starting with the given node,
-    # in an iterative breadth first traversal
-    def bft_print(self):
-        pass
+#     # Print the value of every node, starting with the given node,
+#     # in an iterative depth first traversal
+#     def dft_print(self):
+#         pass
 
-    # Print the value of every node, starting with the given node,
-    # in an iterative depth first traversal
-    def dft_print(self):
-        pass
+#     # Stretch Goals -------------------------
+#     # Note: Research may be required
 
-    # Stretch Goals -------------------------
-    # Note: Research may be required
+#     # Print Pre-order recursive DFT
+#     def pre_order_dft(self):
+#         pass
 
-    # Print Pre-order recursive DFT
-    def pre_order_dft(self):
-        pass
+#     # Print Post-order recursive DFT
+#     def post_order_dft(self):
+#         pass
 
-    # Print Post-order recursive DFT
-    def post_order_dft(self):
-        pass
+# """
+# This code is necessary for testing the `print` methods
+# """
+# bst = BSTNode(1)
 
-"""
-This code is necessary for testing the `print` methods
-"""
-bst = BSTNode(1)
+# bst.insert(8)
+# bst.insert(5)
+# bst.insert(7)
+# bst.insert(6)
+# bst.insert(3)
+# bst.insert(4)
+# bst.insert(2)
 
-bst.insert(8)
-bst.insert(5)
-bst.insert(7)
-bst.insert(6)
-bst.insert(3)
-bst.insert(4)
-bst.insert(2)
+# bst.bft_print()
+# bst.dft_print()
 
-bst.bft_print()
-bst.dft_print()
-
-print("elegant methods")
-print("pre order")
-bst.pre_order_dft()
-print("in order")
-bst.in_order_dft()
-print("post order")
-bst.post_order_dft()  
+# print("elegant methods")
+# print("pre order")
+# bst.pre_order_dft()
+# print("in order")
+# bst.in_order_dft()
+# print("post order")
+# bst.post_order_dft()
